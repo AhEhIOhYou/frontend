5b4eb31846a007e6624716ca8bb66d70e2db402f
ahehiohyou <diman_vladimirov34@mail.ru>
2022-10-10 03:25:42 +0300

    fun 2

---
+++ my-app-svelte/my-best-app/src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let counter = 0;
+  const increment = () => {
+    counter++;
+  }
+
+  $: if (counter > 5) {
+    alert("STOP IT");
+    counter = 0;
+  }
+
+</script>
+
+<button on:click={increment}>
+  COUNTER: {counter}
+</button>
