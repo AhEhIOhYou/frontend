258c56ad56e2cf4cccc7be59f340723bb3e804be
ahehiohyou <diman_vladimirov34@mail.ru>
2022-10-12 22:10:37 +0300

    video control test

--- svelte/my-best-app/src/App.svelte
+++ svelte/my-best-app/src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-  import Counter from './lib/Counter.svelte'
-  import Toggle from "./lib/Toggle.svelte";
-  import Each from "./lib/Each.svelte";
-  import Await from "./lib/Await.svelte";
-  import MouseEvents from "./lib/MouseEvents.svelte";
-  import Inner from "./lib/inner.svelte";
+	import Counter from './lib/Counter.svelte'
+	import Toggle from "./lib/Toggle.svelte";
+	import Each from "./lib/Each.svelte";
+	import Await from "./lib/Await.svelte";
+	import MouseEvents from "./lib/MouseEvents.svelte";
+	import Inner from "./lib/inner.svelte";
 	import Bind from "./lib/Bind.svelte";
+	import VideoControl from "./lib/VideoControl.svelte";
 
 	function handleMessage(event) {
 		console.log(event.detail.text);
@@ -14,24 +15,26 @@
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+	<h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-    <Toggle />
-    <Each />
-    <Await/>
-    <MouseEvents />
-    <Inner on:foo={handleMessage} />
+	<div class="card">
+		<Counter/>
+		<Toggle/>
+		<Each/>
+		<Await/>
+		<MouseEvents/>
+		<Inner on:foo={handleMessage}/>
 		<br>
-		<Bind />
-  </div>
+		<Bind/>
+		<VideoControl />
+	</div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+	<p>
+		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app
+		framework powered by Vite!
+	</p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<p class="read-the-docs">
+		Click on the Vite and Svelte logos to learn more
+	</p>
 </main>
