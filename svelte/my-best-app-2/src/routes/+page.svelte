43be0f6448092606a5cfaadf3d41e6d709bbf38f
ahehiohyou <diman_vladimirov34@mail.ru>
2022-10-23 17:06:06 +0300

    test page.svelte and server.ts

--- svelte/my-best-app-2/src/routes/+page.svelte
+++ svelte/my-best-app-2/src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<form action="hello" method="post">
+		<label>
+			<input type="text" name="important-data">
+		</label>
+		<button type="submit">Press it</button>
+	</form>
+</div>
--- svelte/my-best-app-2/src/routes/what-is-my-user-agent/+server.ts
+++ svelte/my-best-app-2/src/routes/what-is-my-user-agent/+server.ts
@@ -1,5 +1,10 @@
 import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-    return new Response();
+export const GET: RequestHandler = async (event) => {
+    console.log(...event.request.headers);
+    
+    return json({
+        userAgent: event.request.headers.get('user-agent')
+    });
 };
